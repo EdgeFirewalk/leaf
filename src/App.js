2af7870266d154e45e128940c2e606768cb659b2
EdgeFirewalk <thedinsorpaswer2@mail.ru>
2024-09-30 19:11:25 +0600

    Set up Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import HomePage from './pages/HomePage/HomePage';
+import GamePage from './pages/GamePage/GamePage';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<HomePage />}></Route>
+          <Route path="/game" element={<GamePage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
