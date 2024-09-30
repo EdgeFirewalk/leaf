decab30264aa68cec1d25e4c79325229b8db7cd7
EdgeFirewalk <thedinsorpaswer2@mail.ru>
2024-09-30 19:10:53 +0600

    Add pages components

---
+++ src/pages/GamePage/GamePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Container from '../../components/layout/Container/Container';
+
+const GamePage = () => {
+  return (
+    <div>
+      <Container>
+        <p>Game page here...</p>
+      </Container>
+    </div>
+  );
+};
+
+export default GamePage;
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Container from '../../components/layout/Container/Container';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Container>
+        <p>Home page here!</p>
+      </Container>
+    </div>
+  );
+};
+
+export default HomePage;
