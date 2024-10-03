eb38b39bad77246d940571851f693c65c91970ed
EdgeFirewalk <thedinsorpaswer2@mail.ru>
2024-10-03 19:27:34 +0600

    Make game page get the word guessed

--- src/pages/GamePage/GamePage.jsx
+++ src/pages/GamePage/GamePage.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import Container from '../../components/layout/Container/Container';
 
 const GamePage = () => {
+  const location = useLocation();
+
+  const [word, setWord] = useState('');
+
+  useEffect(() => {
+    if (!location.state) {
+      // TODO: Перенаправить на ErrorPage
+    }
+
+    setWord(location.state.word);
+  }, []);
+
   return (
     <div>
       <Container>
-        <p>Game page here...</p>
+        <p>The word is: {word}</p>
       </Container>
     </div>
   );
