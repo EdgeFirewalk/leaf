e503f6871412b2095cf434088d72d4557d4f3dde
EdgeFirewalk <thedinsorpaswer2@mail.ru>
2024-10-03 19:28:22 +0600

    Create home page, add state and functions

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
+import styles from './HomePage.module.scss';
+
+import Leaf from '../../img/leaf.png';
+
+import Modal from '../../components/ui/Modal/Modal';
 import Container from '../../components/layout/Container/Container';
+import Button from '../../components/ui/Button/Button';
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const [wordToGuess, setWordToGuess] = useState('');
+  const [isWordInputModalOpen, setIsWordInputModalOpen] = useState(false);
+
+  const toggleWordInputModal = () => {
+    setIsWordInputModalOpen((isOpen) => !isOpen);
+  };
+
+  const startGame = (withRandomWord) => {
+    if (withRandomWord) {
+      // TODO: Сгенерировать слово и перекинуть на GamePage с ним
+
+      navigate('/game', { state: { word: 'Случайное' } });
+    }
+
+    // TODO: Кинуть на страницу GamePage вместе с wordToGuess
+  };
+
   return (
-    <div>
+    <>
+      <Modal
+        isOpen={isWordInputModalOpen}
+        toggleModal={toggleWordInputModal}
+      ></Modal>
       <Container>
-        <p>Home page here!</p>
+        <div className={styles.page}>
+          <div className={styles.logo}>
+            <img src={Leaf} alt="Leaf icon" />
+            <p className={styles.logoText}>Лист</p>
+          </div>
+          <div className={styles.buttons}>
+            <Button className={styles.button} onClick={(e) => startGame(true)}>
+              Случайное слово
+            </Button>
+            <Button
+              className={styles.button}
+              onClick={(e) => toggleWordInputModal()}
+            >
+              Загадать слово
+            </Button>
+          </div>
+        </div>
       </Container>
-    </div>
+    </>
   );
 };
 
